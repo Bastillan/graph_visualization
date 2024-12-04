e29edad32226f9f19d5fb1b04282b03f89195c4b
mpienkos <michal.pienkos@op.pl>
2024-12-04 14:33:47 +0100

    test gui

--- cpp/app/math.cpp
+++ cpp/app/math.cpp
@@ -18,10 +18,15 @@ float divide(float arg1, float arg2) {
     return arg1 / arg2;
 }
 
+float y(float x, float a, float b) {
+    return a * x + b;
+}
+
 PYBIND11_MODULE(calculations, handle) {
     handle.doc() = "This is the module docs.";
     handle.def("add", &add);
     handle.def("sub", &sub);
     handle.def("mul", &mul);
     handle.def("div", &divide);
+    handle.def("y", &y);
 }
