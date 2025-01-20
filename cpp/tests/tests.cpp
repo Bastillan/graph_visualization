71df78d38ee1950b8b51d35a69edc5481a0942cd
mpienkos <michal.pienkos@op.pl>
2025-01-21 00:04:41 +0100

    started implementing tests

--- cpp/tests/tests.cpp
+++ cpp/tests/tests.cpp
@@ -1,1 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
+#include "graph_manager.hpp"
+
+TEST_CASE("LoadGraph test", "[graph_manager]") {
+    auto graph = GraphManager::loadGraph("./tests/test_graphs/graph1.graphml");
+    auto edges = GraphManager::getEdges(graph);
+}
